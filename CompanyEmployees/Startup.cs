7053fed8ee31396710786e70267e39b11b480c96
kristimone <kristi.mone23@gmail.com>
2022-04-04 21:52:17 +0200

    Content Negotiations

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -46,6 +46,12 @@ namespace CompanyEmployees
             {
                 x.SwaggerDoc("v1", new OpenApiInfo { Title = "CompanyEmployees API", Version = "v1" });
             });
+            services.AddControllers(config =>
+            {
+                config.RespectBrowserAcceptHeader = true;
+                config.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters()
+            .AddCustomCSVFormatter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
